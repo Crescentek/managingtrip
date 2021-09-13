b78ee8d4c69456d1073f3b4e533c18e9028bcda4
sumit-max-arch <sumitchowdhary01@gmail.com>
2021-09-13 12:10:17 +0530

    Trips Mangement completed

--- pages/index.jsx
+++ pages/index.jsx
@@ -1,17 +1,16 @@
-import Head from 'next/head'
-import SimpleLayout from '../components/layout/simple'
+import Login from "../components/auth";
+import SimpleLayout from "../components/layout/simple";
 
-export default function Home(initialData) {
+export default function Home() {
   return (
     <SimpleLayout>
       <section className="jumbotron text-center">
         <div className="container">
           <h1>Book Your Ticket Trip Now</h1>
-          <p className="lead text-muted">
-            Buy Now
-          </p>
+          <p className="lead text-muted">Buy Now</p>
+          <Login/>
         </div>
       </section>
     </SimpleLayout>
-  )
+  );
 }
